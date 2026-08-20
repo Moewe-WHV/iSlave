43fc25e849d54a1fc9a2da5097f251c34cb391a9
stpeppo <st.peppo@googlemail.com>
2026-08-20 09:25:14 +0200

    final Variante a

--- docs/code/akku_1.py
+++ docs/code/akku_1.py
@@ -1,10 +1,10 @@
 class Akku:
-    def __init__(self, akkustand: int):
+    def __init__(self, akkustand: int): #Kostruktor Akkustand sofort gültig
         self.akkustand = akkustand
 
-    def akkustand_anzeigen(self) -> int:
+    def akkustand_anzeigen(self) -> int: #Methode
         print(f"Akkustand: {self.akkustand}%")
         return self.akkustand
 
-mein_akku = Akku(100)
-mein_akku.akkustand_anzeigen()
+mein_akku = Akku(100) #Objekt
+mein_akku.akkustand_anzeigen() #Ausgabe
