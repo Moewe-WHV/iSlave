ee43ff7473b244afc52ba268bce68b9799a64d1b
Tim K <klein_tim@t-online.de>
2026-08-20 20:07:40 +0200

    main.py

--- src/akku1.py
+++ src/akku1.py
@@ -32,4 +32,3 @@ weitere_aufgabe = "ja"
 while weitere_aufgabe == "ja":  #Abfrage ob noch eine Aufgabe erledigt werden soll
     mein_akku.aufgabe()
     weitere_aufgabe = input("Soll noch eine weitere Aufgabe ausgeführt werden? (ja / nein)")
-
