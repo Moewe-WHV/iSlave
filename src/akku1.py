e58ae8d31318e4a99ac266c55d01844e5794edec
Tim K <klein_tim@t-online.de>
2026-08-20 13:58:02 +0200

    test_akku1.py import angepasst

--- src/akku1.py
+++ src/akku1.py
@@ -25,12 +25,11 @@ class Akku:
             print("Akku zu niedrig, Aufgabe kann nicht ausgeführt werden.")
 
 
-if __name__ == "__main__":
-    mein_akku = Akku(100)           #Akku objekt erstellt
-    mein_akku.akkustand_anzeigen()  #Akkustand anzeigen
-    weitere_aufgabe = "ja"
+mein_akku = Akku(100)           #Akku objekt erstellt
+mein_akku.akkustand_anzeigen()  #Akkustand anzeigen
+weitere_aufgabe = "ja"
 
-    while weitere_aufgabe == "ja":  #Abfrage ob noch eine Aufgabe erledigt werden soll
-        mein_akku.aufgabe()
-        weitere_aufgabe = input("Soll noch eine weitere Aufgabe ausgeführt werden? (ja / nein)")
+while weitere_aufgabe == "ja":  #Abfrage ob noch eine Aufgabe erledigt werden soll
+    mein_akku.aufgabe()
+    weitere_aufgabe = input("Soll noch eine weitere Aufgabe ausgeführt werden? (ja / nein)")
 
