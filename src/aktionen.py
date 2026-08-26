7e4d2ba5e7f40ebeec99c20457b67b573bb894a4
stpeppo <st.peppo@googlemail.com>
2026-08-26 13:24:43 +0200

    aktionen.py erstellt un dfinished / main_interaktion_2.py erstellt und finished

--- src/aktionen.py
+++ src/aktionen.py
@@ -1,44 +1,15 @@
-from akku1 import Akku 
+class RoboterAktionen:
 
+    gueltige_aktionen = ["Spülmittel", "Staubsaugeraufsätze"]
 
-#raum = input("Wohin?")  # raum 
-aktion = input("was?")  # Aktion
+    def __init__(self):
+        self.aktuelle_aktionen= None
+ 
+    def aktion_ausfuehren(self, aktion):
+        if aktion in self.gueltige_aktionen:
+            self.gueltige_aktionen = aktion
+            return aktion
+        else: 
+            return "Aktion nicht möglich"
 
-
-class Roboter():
-
-    def spuellmittel_aktion1(self, akku_objekt):
-        print("Spuellmittel Aktion 1 wird ausgeführt")
-        akku_objekt.aufgabe()
-    
-        
-    def spuellmittel_aktion2():
-        pass
-
-    def staubsauger_aktion1():
-        pass
-
-    def staubsauger_aktion2():
-        pass
-
-
-
-
-class Spuellmittel:
-
-    def __init__(self, anzahl):
-        self.anzahl = anzahl
-
-
-class Staubsauger:
-
-    def __init__(self, aufsaetze):
-        self.aufsaetze = aufsaetze
-    
-roboter = Roboter()    
-akku_test = Akku(100)
-
-methode = getattr(roboter, aktion, None)
-methode(akku_test)
-
-#roboter.spuellmittel_aktion1(akku_test)
+ 
