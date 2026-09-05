a89a8b81f2b79b644d9e31a3d2521bedbae477ae
Tim K <klein_tim@t-online.de>
2026-09-05 18:22:20 +0200

    fix: black formatierung

--- src/wartung.py
+++ src/wartung.py
@@ -9,9 +9,8 @@ def ist_wartung_faellig(wartungstermin: date, aktuelles_datum: date) -> bool:
     return aktuelles_datum >= wartungstermin
 
 
-
 def berechne_verbleibende_tage(wartungstermin, aktuelles_datum) -> int:
-     return (wartungstermin - aktuelles_datum).days
+    return (wartungstermin - aktuelles_datum).days
 
 
 def berechne_verbleibende_tage(wartungstermin: date, aktuelles_datum: date) -> int:
